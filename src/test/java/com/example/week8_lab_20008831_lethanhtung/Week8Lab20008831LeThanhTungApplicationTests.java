41d9a500cae8c85a3d58308756e57f84d9334f83
main <booking.tungcutte@gmail.com>
2023-11-25 10:14:51 +0700

    build init

---
+++ src/test/java/com/example/week8_lab_20008831_lethanhtung/Week8Lab20008831LeThanhTungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week8_lab_20008831_lethanhtung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week8Lab20008831LeThanhTungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
