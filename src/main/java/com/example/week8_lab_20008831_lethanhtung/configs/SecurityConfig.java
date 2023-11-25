7d8abae55190edb93f3736abc8c1e7eaa0d645ef
main <booking.tungcutte@gmail.com>
2023-11-25 11:07:03 +0700

    fix Authorities

--- src/main/java/com/example/week8_lab_20008831_lethanhtung/configs/SecurityConfig.java
+++ src/main/java/com/example/week8_lab_20008831_lethanhtung/configs/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.week8_lab_20008831_lethanhtung.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,46 +14,55 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
     @Autowired
-    public void globalConfig(AuthenticationManagerBuilder auth, PasswordEncoder
-            encoder)throws Exception{
+    public void globalConfig(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = passwordEncoder();
+
         auth.inMemoryAuthentication()
                 .withUser(User.withUsername("admin")
-                        .password(encoder.encode("admin"))
+                        .password(passwordEncoder.encode("admin"))
                         .roles("ADMIN")
                         .build())
                 .withUser(User.withUsername("teo")
-                        .password(encoder.encode("teo"))
+                        .password(passwordEncoder.encode("teo"))
                         .roles("TEO")
                         .build())
                 .withUser(User.withUsername("ty")
-                        .password(encoder.encode("ty"))
+                        .password(passwordEncoder.encode("ty"))
                         .roles("USER")
-                        .build())
-        ;
+                        .build());
     }
+
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth->auth
-                        .requestMatchers("/","/home","/index").permitAll()//nhung links nay khong can authenticate
-                        .requestMatchers("/api/**").hasAnyRole("ADMIN","USER","TEO")//nhung uri bat dau bang /api can phai dang nhap voi cac role admin/user/teo
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers(("/admin/**")).hasRole("ADMIN")//uri bat dau bang admin thi phai dang nhap voi quyen admin
-                        .anyRequest().permitAll()
-                //.authenticated()//cac uri khac can dang nhap duoi bat ky ole nao
-        );
-        http.csrf(csrf->csrf.ignoringRequestMatchers("/h2-console/**"));
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/home", "/index").permitAll() // These links don't require authentication
+                .requestMatchers("/api/**").hasAnyRole("ADMIN", "USER", "TEO") // URIs starting with /api require any of the ADMIN, USER, or TEO roles
+                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN") // URIs starting with /admin require the ADMIN role
+                .anyRequest().permitAll()); // Allow access to any other URL without authentication
+
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));
         http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
-        http.httpBasic(Customizer.withDefaults());//cac thiet lap con lai thi themac dinh
+        http.httpBasic(Customizer.withDefaults()); // Use default HTTP Basic configuration for other settings
+
         return http.build();
     }
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
